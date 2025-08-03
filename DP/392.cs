58a20b83c5f54e7723f0ecfed2fd4aa64d4547d3
virus27tk <virus27.tk1998@gmail.com>
2025-08-03 20:40:39 +0530

    Problem Solved - 392

---
+++ DP/392.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        int res = 0;
+
+        if (s.Length == 0)
+        {
+            return true;
+        }
+
+        for(int i =0; i<t.Length; i++)
+        {
+            if (t[i] == s[res]){
+                res++;
+            }
+
+            if (res == s.Length){
+                return true;
+            }
+        }
+
+        return res == s.Length;
+    }
+}
