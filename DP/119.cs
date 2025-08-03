34f6649e08031c83ab7fe1d7aaa9fadccb8e099e
virus27tk <virus27.tk1998@gmail.com>
2025-08-03 17:39:26 +0530

    Problem Solved - 199

---
+++ DP/119.cs
@@ -0,0 +1,35 @@
+﻿public class Solution {
+    public IList<int> GetRow(int rowIndex) {
+        if (rowIndex == 0){
+            return new List<int>{1};
+        }
+
+        if (rowIndex == 1){
+            return new List<int>{1,1};
+        }
+
+        var result = new List<int>{1,1};
+
+        for (int i = 2; i <=rowIndex; i++)
+        {
+            int inter = -1;
+            for(int j=1; j<=i; j++){
+                if (j == i){
+                    result[j-1] = inter;
+                    result.Add(1);
+                }
+                else
+                {
+                    var temp = result[j] + result[j-1];
+                    if (inter != -1){
+                        result[j-1] = inter;
+                    }
+                    inter = temp;
+                }
+            }
+        }
+
+        return result;
+        
+    }
+}
