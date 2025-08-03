54ec674c1e8365b1081cf5cd06ae9b67a41e36a7
virus27tk <virus27.tk1998@gmail.com>
2025-08-03 16:59:47 +0530

    Problem Solved - 118
    Problem Solution + Folder restructure for Dynamic Programming

---
+++ DP/118.cs
@@ -0,0 +1,32 @@
+﻿public class Solution {
+    public IList<IList<int>> Generate(int numRows) {
+        var result = new List<IList<int>>();
+        result.Add(new List<int>{ 1 });
+        if (numRows == 1){
+            return result;
+        }
+
+        result.Add(new List<int>{ 1, 1 });
+        if (numRows == 2){
+            return result;
+        }
+
+        for (int i = 2; i<numRows; i++){
+            var inter = new List<int>();
+            for(int j=0; j<i+1; j++){
+                if (j == 0 || j==i){
+                    inter.Add(1);
+                }
+                else
+                {
+                    var valueToAdd = result[i-1][j] + result[i-1][j-1];
+                    inter.Add(valueToAdd);
+                }
+            } 
+
+            result.Add(inter);
+        }
+
+        return result;
+    }
+}
