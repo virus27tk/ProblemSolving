b233fe6b04efcb7f344eaa063c08d15ea822e89c
virus27tk <virus27.tk1998@gmail.com>
2025-08-09 23:05:43 +0530

    Problem Solved - 647

---
+++ DP/647.cs
@@ -0,0 +1,27 @@
+﻿public class Solution {
+    public int CountSubstrings(string s) 
+    {
+        int result = 0;
+        for (int k=0; k<s.Length; k++)
+        {
+            int i=k;
+            int j=k;
+            while(i >=0 && j<s.Length && s[i] == s[j])
+            {
+                result++;
+                i--;j++;
+            }
+
+            i=k;
+            j=k+1;
+            while(i >=0 && j<s.Length && s[i] == s[j])
+            {
+                result++;
+                i--;j++;
+            }
+        }
+
+        return result;
+        
+    }
+}
